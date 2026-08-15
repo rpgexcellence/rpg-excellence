85c4b6ce11c7031aab51fc2157572cc26148bd7c
rpgexcellence <info@rpgexcellence.com>
2026-08-16 00:20:25 +0100

    Fix Supabase server import

--- app/portal/login/actions.js
+++ app/portal/login/actions.js
@@ -1,7 +1,7 @@
 "use server";
 
 import { redirect } from "next/navigation";
-import { createClient } from "@/lib/supabase/server";
+import { createClient } from "../../../lib/supabase/server";
 
 export async function signIn(formData) {
   const supabase = await createClient();
