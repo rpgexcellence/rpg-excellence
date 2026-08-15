6d834897188f5c7ca7a4671f711acaa2684716ff
rpgexcellence <info@rpgexcellence.com>
2026-08-15 18:30:32 +0100

    Integrate Cookie and Analytics consent components
    Added CookieConsent and AnalyticsConsent components to the layout.

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
-
+import CookieConsent from "../components/CookieConsent";
+import AnalyticsConsent from "../components/AnalyticsConsent";
 export const metadata = {
   metadataBase: new URL("https://www.rpgexcellence.com"),
   title: {
@@ -48,7 +49,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+  {children}
+     <AnalyticsConsent />
+  <CookieConsent />
+    </body>
     </html>
   );
 }
